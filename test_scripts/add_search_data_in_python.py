fdbb83a36eb2e4bd656b7fc5cbb251c5fedc1f8a
Ritika Srivastava <ritika86.srivastava@gmail.com>
2021-05-27 13:30:08 +0200

    feat:test hound code review

---
+++ test_scripts/add_search_data_in_python.py
@@ -0,0 +1,24 @@
+# add_search_data_in_python.py
+
+# number set definition
+number_set = {    23, 90, 56, 78, 12, 34, 67 }
+
+# new data is added
+
+number_set.add(50)
+
+# set values printing
+print(number_set)
+
+_message = "that number is not available."
+
+#request use for a number value for search
+search_val = int(input("input a number:"))
+#Search the number in the set
+for val in number_set:
+  if val = search_val:
+
+   _message = "that number is available."
+break
+
+print(_message)
