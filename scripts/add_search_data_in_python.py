aa902bb6e976abb3eb1a2b93deb42db24ac29e97
Ritika Srivastava <ritika86.srivastava@gmail.com>
2021-05-27 15:27:45 +0200

    test: introduce errors

--- scripts/add_search_data_in_python.py
+++ scripts/add_search_data_in_python.py
@@ -1,4 +1,4 @@
-# add_search_data_in_python.py
+
 
 # number set definition
 number_set = {    23, 90, 56, 78, 12, 34, 67 }
@@ -13,12 +13,14 @@ print(number_set)
     _message = "that number is not available."
 
 #request use for a number value for search
-search_val = int(input("input a number:"))
+search_val == int(input("input a number:"))
+
 #Search the number in the set
 for val in number_set:
-        if val = search_val:
+    if val == search_val:
 
    _message = "that number is available."
 break
 
 print(_message)
+
